8935973073516022f85a9f003afdbf143981cc91
jhyeo <icepri3535@gmail.com>
2021-12-28 15:27:44 +0900

    SpringBoard 셋팅 완료

--- src/main/java/org/example/springboard/BoardController.java
+++ src/main/java/org/example/springboard/BoardController.java
@@ -1,13 +1,23 @@
 package org.example.springboard;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+@Controller//주소값 맵핑을 할수 있는 특별한 Bean
+@RequestMapping("/board")//1차 주소값 만들때 이거밖에 못 쓴다.
 public class BoardController {
+    @Autowired
+    private BoardService service;
 
-    @RequestMapping("/list")
+    @GetMapping("/list")
     public void list() {
+
+    }
+
+    @GetMapping("/write")
+    public void write() {
 
     }
 }

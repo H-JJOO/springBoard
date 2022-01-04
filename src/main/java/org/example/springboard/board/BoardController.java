33d1f3a10f17bd63b580bda41f3403d028e803b9
jhyeo <icepri3535@gmail.com>
2022-01-04 16:00:56 +0900

    SpringBoard 셋팅 완료 후 게시판 3일차 (CRUD XML 약간 수정, & 로그인 & Const 사용해서 문자열입력할때 오타발생하지 않게 Class 에 사전에 등록해서 사용, ENUM 대체가능, CRUD 수정, 회원가입까지 완료)

--- src/main/java/org/example/springboard/board/BoardController.java
+++ src/main/java/org/example/springboard/board/BoardController.java
@@ -22,7 +22,7 @@ public class BoardController {
 
 
     @GetMapping("/list")
-    public void list(Model model) {
+    public void list(Model model) {//request.setAttribute 와 비슷한 역할
         model.addAttribute("list", service.selBoardList());
     }
 
@@ -33,30 +33,30 @@ public class BoardController {
     }
 
     @GetMapping("/write")
-    public void write() {}
+    public void write() {}//board/write 로 이동
 
     @PostMapping("/write")
     public String writeProc(BoardEntity entity, RedirectAttributes reAttr) {
         int result = service.insBoard(entity);
-        if (result == 0) {
+        if (result == 0) {//글쓰기 실패했을때
 //            RedirectAttributes 는 redirect 와 관련되어 있다.
 //            reAttr.addAttribute("msg", "글 등록에 실패하였습니다."); // addAttribute 는 쿼리스트링 생성
-            reAttr.addFlashAttribute("msg", "글 등록에 실패하였습니다.");//객체로 보내지네?(req.setAttribute 같이, request에 담아서 보내준다.)
+            reAttr.addFlashAttribute("msg", "글 등록에 실패하였습니다.");//객체로 보내지내?(req.setAttribute 같이, request에 담아서 보내준다.)
             reAttr.addFlashAttribute("data", entity);
-            return "redirect:/board/write";
+            return "redirect:/board/write";//다시 글쓰기 창으로 이동
         }
-        return "redirect:/board/list";
+        return "redirect:/board/list";//글쓰기 성공하면 list 로 이동
     }
 
     @GetMapping("/del")
     public String delProc(BoardEntity entity, RedirectAttributes reAttr) {
         int result = service.delBoard(entity);
-        if (result == 0) {
+        if (result == 0) {//글삭제 실패하면
             reAttr.addFlashAttribute("msg", "글 삭제에 실패하였습니다.");
             reAttr.addAttribute("iboard", entity.getIboard());//쿼리스트링 iboard = pk값
-            return "redirect:/board/detail";
+            return "redirect:/board/detail";//삭제 실패한 detail 로 이동
         }
-        return "redirect:/board/list";
+        return "redirect:/board/list";//삭제 성공하면 list 로 이동
     }
 
     @GetMapping("/mod")
